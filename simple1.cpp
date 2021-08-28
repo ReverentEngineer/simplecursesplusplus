8bf490be3b6d6fd0faca4147e26ea3677e4f5223
Jeff Caffrey-Hill <jeff@caffreyhill.com>
2021-08-28 17:31:49 -0400

    Format with clang-format

--- simple1.cpp
+++ simple1.cpp
@@ -5,13 +5,12 @@ using namespace SimpleCurses;
 int main() {
   try {
     Screen screen;
-	screen.add("field", std::make_unique<Text>(10, 10, "My awesome app"));
+    screen.add("field", std::make_unique<Text>(10, 10, "My awesome app"));
     screen.update();
     getchar();
   } catch (const CursesException &ex) {
     std::cerr << ex.what() << std::endl;
   }
-
 
   return 0;
 }
--- simplecurses++.hh
+++ simplecurses++.hh
@@ -4,7 +4,7 @@
  *
  * This is a header-only library. All one needs to do is to include the
  * header in the code:
- * 
+ *
  * @code
  * #include <simplecurses++.hh>
  * @endcode
@@ -49,15 +49,13 @@ private:
  */
 class Element {
 public:
-  
   /**
    * @brief Constructor
    *
    * @param x The x coordinate relative to its container
    * @param y The y coordinate relative to its container
    */
-  Element(int x, int y)
-      :  m_x(x), m_y(y) {}
+  Element(int x, int y) : m_x(x), m_y(y) {}
 
   /**
    * @brief Destructor
@@ -80,10 +78,9 @@ public:
    *
    * @param The parent window.
    */
-  virtual void draw(WINDOW* parent) = 0;
+  virtual void draw(WINDOW *parent) = 0;
 
 private:
-
   /** @brief The x coordinate **/
   int m_x;
 
@@ -92,68 +89,60 @@ private:
 };
 
 class Text : public Element {
-  public:
-    Text(int x, int y, const std::string &text)
-        : Element(x, y), m_text(text) {}
+public:
+  Text(int x, int y, const std::string &text) : Element(x, y), m_text(text) {}
 
-    virtual ~Text() = default;
+  virtual ~Text() = default;
 
-	/**
-	 * @brief Gets the text of the element
-	 */
-	const std::string& text() const { return m_text; }
+  /**
+   * @brief Gets the text of the element
+   */
+  const std::string &text() const { return m_text; }
 
-    /**
-     * @copydoc Element::rows()
-     */
-    int rows() const { return 1; }
+  /**
+   * @copydoc Element::rows()
+   */
+  int rows() const { return 1; }
 
-    /**
-     * @copydoc Element::cols()
-     */
-    int cols() const { return m_text.size(); }
-  
-	void draw(WINDOW* parent) {
-		mvwaddstr(parent, y(), x(), m_text.c_str());
-	}
+  /**
+   * @copydoc Element::cols()
+   */
+  int cols() const { return m_text.size(); }
 
-  private:
-    std::string m_text;
-  };
+  void draw(WINDOW *parent) { mvwaddstr(parent, y(), x(), m_text.c_str()); }
 
-
+private:
+  std::string m_text;
+};
 
 /**
  * @brief A window in the interface
  */
 class Window : public Element {
 public:
-
   Window(int x, int y, int rows, int cols, bool border)
-      : Element(x, y), m_rows(rows), m_cols(cols), m_border(border), m_window(NULL) {
-	if (m_border) {
-		m_rows--;
-		m_cols--;
-	}
+      : Element(x, y), m_rows(rows), m_cols(cols), m_border(border),
+        m_window(NULL) {
+    if (m_border) {
+      m_rows--;
+      m_cols--;
+    }
   }
 
-
-  /** 
+  /**
    * @brief Destructor
    */
-  virtual ~Window() {
-	delwin(m_window);
-  }
+  virtual ~Window() { delwin(m_window); }
 
   /**
    * @brief Updates the screen with the changes in the window
    */
-  void update() { 
-	  //std::for_each(m_subwindows.begin(), m_subwindows.end(), [&](WindowPtr& window) {
-	  //		window->update();
-	  //});
-	  touchwin(m_window);
-	  wrefresh(m_window);
+  void update() {
+    // std::for_each(m_subwindows.begin(), m_subwindows.end(), [&](WindowPtr&
+    // window) { 		window->update();
+    //});
+    touchwin(m_window);
+    wrefresh(m_window);
   }
 
   /**
@@ -166,32 +155,31 @@ public:
    */
   int cols() const { return m_cols; }
 
-
-  void draw(WINDOW* parent) {
-	if (!m_window) {
-    	m_window = derwin(parent, m_cols, m_rows, y(), x());
-	}
-	if (m_border) {
-		box(m_window, 0, 0);
-	}
+  void draw(WINDOW *parent) {
+    if (!m_window) {
+      m_window = derwin(parent, m_cols, m_rows, y(), x());
+    }
+    if (m_border) {
+      box(m_window, 0, 0);
+    }
   }
 
-
   template <typename WindowElement>
-  WindowElement& add(const std::string& name, std::unique_ptr<WindowElement> element) {
-	if (!element) {
-		throw CursesException("No element provided.");
-	}
+  WindowElement &add(const std::string &name,
+                     std::unique_ptr<WindowElement> element) {
+    if (!element) {
+      throw CursesException("No element provided.");
+    }
 
-	if ((element->x() + element->cols() > m_cols) ||
-		 (element->y() + element->rows() > m_rows))	{
-		throw CursesException("Element doesn't fit in window");
-	}
+    if ((element->x() + element->cols() > m_cols) ||
+        (element->y() + element->rows() > m_rows)) {
+      throw CursesException("Element doesn't fit in window");
+    }
 
-	WindowElement& ref = *element.get();
-	ref.draw(m_window);
-	m_elements[name] = std::move(element);
-	return ref;
+    WindowElement &ref = *element.get();
+    ref.draw(m_window);
+    m_elements[name] = std::move(element);
+    return ref;
   }
 
   /**
@@ -200,35 +188,33 @@ public:
    * @param name The name of the element to remove.
    */
   void remove(const std::string &name) {
-	const ElementPtr& element = m_elements.at(name);
-	for (size_t i = element->y(); i < element->rows(); i++) {
-		wmove(this->m_window, i, element->x());
-		for (int j = 0; j < element->cols(); j++) {
-			waddch(this->m_window, ' ');
-		}
-	}
-	if (m_border) {
-		if (element->x() == 0 || element->y() == 0 ||
-			(element->x() + element->cols()) == (m_cols - 1) ||
-			(element->y() + element->rows()) == (m_rows - 1)) {
-			draw(NULL); // Parent not necessary, just redrawing border.
-		}
-	}
-  	m_elements.erase(name);
+    const ElementPtr &element = m_elements.at(name);
+    for (size_t i = element->y(); i < element->rows(); i++) {
+      wmove(this->m_window, i, element->x());
+      for (int j = 0; j < element->cols(); j++) {
+        waddch(this->m_window, ' ');
+      }
+    }
+    if (m_border) {
+      if (element->x() == 0 || element->y() == 0 ||
+          (element->x() + element->cols()) == (m_cols - 1) ||
+          (element->y() + element->rows()) == (m_rows - 1)) {
+        draw(NULL); // Parent not necessary, just redrawing border.
+      }
+    }
+    m_elements.erase(name);
   }
 
 protected:
-
   /**
    * @brief Protected constructor
    *
    * This exists solely to allow Screen to be a special type of Window.
    */
-  Window(int x, int y, WINDOW* window) :
-   	Element(x, y), m_window(window) {
-	getmaxyx(window, m_cols, m_rows); 
+  Window(int x, int y, WINDOW *window) : Element(x, y), m_window(window) {
+    getmaxyx(window, m_cols, m_rows);
   }
- 
+
 private:
   typedef std::unique_ptr<Window> WindowPtr;
   typedef std::unique_ptr<Element> ElementPtr;
@@ -253,11 +239,11 @@ public:
 
   virtual ~Screen() { endwin(); }
 
-  virtual void draw(WINDOW* window) {
-	throw CursesException("Screen should never be drawn.");
+  virtual void draw(WINDOW *window) {
+    throw CursesException("Screen should never be drawn.");
   }
 };
 
-}
+} // namespace SimpleCurses
 
 #endif /* SIMPLECURSESPP_HH */
